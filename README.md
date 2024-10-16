a81708c36e8e8d2d5c0d8782ff99d52d91cea719
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-10-16 14:19:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Mitex Extension For Quarto
+
+This is a Quarto extension that provides support for using the [Mitex](https://typst.app/universe/package/mitex/) Typst library to render mathematical expressions.
+
+## Installing
+
+```bash
+quarto add quarto-ext/mitex
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+To use the extension, add the following to your Quarto document:
+
+```yaml
+filters: 
+  - mitex
+```
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
---
+++ _extensions/mitex/mitex.lua
@@ -0,0 +1,33 @@
+local math_activated = false
+
+local function ensure_mitex()
+  if not math_activated then
+    quarto.doc.include_text('in-header', '#import "@preview/mitex:0.2.4": *')
+    math_activated = true
+  end
+end
+
+-- Inline Math
+Math = function(m)
+  if not quarto.doc.is_format('typst') then
+    return nil
+  end
+  ensure_mitex()
+  quarto.log.output(m)
+  if m.mathtype == "InlineMath" then
+    return pandoc.RawInline('typst', '#mi("' .. m.text .. '")')
+  end
+end
+
+-- DisplayMath 
+-- We want to replace all Para by a RawBlock
+Para = function(p)
+  if not quarto.doc.is_format('typst') then
+    return nil
+  end
+  ensure_mitex()
+  if p.content[1].t == "Math" and p.content[1].mathtype == "DisplayMath" then
+    return pandoc.RawBlock('typst', '#mitex(`' .. p.content[1].text .. '`)')
+  end
+end
+
